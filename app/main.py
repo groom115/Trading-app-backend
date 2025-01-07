de822a2c45dea1588ce505c07bfe523b3e751b8e
groom115 <shyamabhishek115@gmail.com>
2025-01-06 20:50:42 -0500

    commit

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
-# app/main.py
-from fastapi import FastAPI
-# from app.routes import user_routes
+from app import create_app
+# from app.api import auth, random_number, file_operations, streaming
 
-app = FastAPI()
+app = create_app()
 
-# Registering the routes
-# app.include_router(user_routes.router)
+# Include routers
+# app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+# app.include_router(random_number.router, prefix="/random", tags=["Random Numbers"])
+# app.include_router(file_operations.router, prefix="/csv", tags=["CSV Operations"])
+# app.include_router(streaming.router, prefix="/stream", tags=["Streaming"])
 
 @app.get("/")
 def read_root():
